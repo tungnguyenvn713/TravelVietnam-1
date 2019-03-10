5d29f1c2c95a6ee4d3e38f17c5cdb3e4ce600c5f
baihocdautien <tungnguyenvn713@gmail.com>
2019-03-10 12:39:56 +0700

    fist cmt

---
+++ app/src/main/java/com/example/admin/demomyvietnam/ActionFragment/QuanLyActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.demomyvietnam.ActionFragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.demomyvietnam.R;
+
+public class QuanLyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly);
+    }
+}
